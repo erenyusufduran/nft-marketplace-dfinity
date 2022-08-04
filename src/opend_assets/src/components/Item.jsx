d8b48dcf0c87d33eb2194760e477eb250361d919
Eren Yusuf Duran <erenyusufduran@gmail.com>
2022-08-04 11:41:45 +0300

    viewing nft on the frontend

--- src/opend_assets/src/components/Item.jsx
+++ src/opend_assets/src/components/Item.jsx
@@ -1,20 +1,53 @@
-import React from "react";
-import logo from "../../assets/logo.png";
+import React, { useEffect, useState } from "react";
+import { Actor, HttpAgent } from "@dfinity/agent";
+import { idlFactory } from "../../../declarations/nft";
+import { Principal } from "@dfinity/principal";
 
-function Item() {
+function Item(props) {
+  const [name, setName] = useState();
+  const [ownerId, setOwnerId] = useState();
+  const [image, setImage] = useState();
+
+  const id = Principal.fromText(props.id);
+  const localhost = "http://localhost:8080";
+  const agent = new HttpAgent({ host: localhost });
+
+  async function loadNFT() {
+    const NFTActor = await Actor.createActor(idlFactory, {
+      agent,
+      canisterId: id,
+    });
+    const _name = await NFTActor.getName();
+    const _id = await NFTActor.getOwner();
+    const imageData = await NFTActor.getAsset();
+    const imageContent = new Uint8Array(imageData);
+    const _image = URL.createObjectURL(
+      new Blob([imageContent.buffer], { type: "image/png" })
+    );
+
+    setName(_name);
+    setOwnerId(_id.toText());
+    setImage(_image);
+  }
+
+  useEffect(() => {
+    loadNFT();
+  }, []);
+
   return (
     <div className="disGrid-item">
       <div className="disPaper-root disCard-root makeStyles-root-17 disPaper-elevation1 disPaper-rounded">
         <img
           className="disCardMedia-root makeStyles-image-19 disCardMedia-media disCardMedia-img"
-          src={logo}
+          src={image}
         />
         <div className="disCardContent-root">
           <h2 className="disTypography-root makeStyles-bodyText-24 disTypography-h5 disTypography-gutterBottom">
-            CryptoDunks #312<span className="purple-text"></span>
+            {name}
+            <span className="purple-text"></span>
           </h2>
           <p className="disTypography-root makeStyles-bodyText-24 disTypography-body2 disTypography-colorTextSecondary">
-            Owner: sdfsdf-erwerv-sdf
+            Owner: {ownerId}
           </p>
         </div>
       </div>
